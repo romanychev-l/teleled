b46d7c0d155cf68f33c20934a2632c87702a40de
Леонид <romanychev-l@users.noreply.github.com>
2021-05-11 18:14:31 +0300

    Init commit. Connect to Wi-Fi

---
+++ main.ino
@@ -0,0 +1,44 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+// Replace with your network credentials
+#define WIFI_SSID "MVMV"
+#define WIFI_PASSWORD "PMPUtheBEST"
+
+WiFiClientSecure secured_client;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  // attempt to connect to Wifi network:
+  configTime(0, 0, "pool.ntp.org");      // get UTC time via NTP
+  secured_client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Check NTP/Time, usually it is instantaneous and you can delete the code below.
+  Serial.print("Retrieving time: ");
+  time_t now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    Serial.print(".");
+    delay(100);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+}
+
+void loop()
+{
+}
