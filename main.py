0861d2781172bceb988bd5b2ad74a9a8a229abf7
Leonid <leonid20.0831@gmail.com>
2021-12-30 15:48:24 +0300

    Fix

--- main.py
+++ main.py
@@ -17,8 +17,17 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
+import json
+import requests
+from requests.structures import CaseInsensitiveDict
 
-pixels = neopixel.NeoPixel(board.D18,100)
+url = "https://api.weather.yandex.ru/v2/informers?lat=59.873546&lon=29.827624&lang=ru_RU"
+
+headers = CaseInsensitiveDict()
+headers["X-Yandex-API-Key"] = "a49c927e-27e2-4089-bf2e-0c865e207104"
+
+
+pixels = neopixel.NeoPixel(board.D18,100,auto_write=False,)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -51,11 +60,11 @@ em_kb = [KeyboardButton(i) for i in em]
 kb.row(em_kb[0], em_kb[1], em_kb[2])
 kb.row(em_kb[3], em_kb[4], em_kb[5])
 kb.row(em_kb[6], em_kb[7], em_kb[8])
+kb.row(KeyboardButton('🌈'), KeyboardButton('🌡'))
 
 
 def change_digit(t):
-    num = nums[t]
-    
+    print('change_digit', t)
     current_color = 0
     with open('color.txt', 'r') as f:
         line = f.read()
@@ -66,6 +75,25 @@ def change_digit(t):
 
             current_color = (int(l[0]), int(l[1]), int(l[2]))
 
+
+
+    if isinstance(t, str):
+        if t == '-':
+            for i in range(98):
+                if i >= 84:
+                    pixels[i] = current_color
+                else:
+                    pixels[i] = (0, 0, 0)
+            pixels.show()
+            return
+        try:
+            t = int(t)
+        except Exception as e:
+            print(e)
+            return
+
+    num = nums[t]
+    
     for i in range(1, 8):
         for j in range(14):
             if i in num:
@@ -74,6 +102,7 @@ def change_digit(t):
                 pixels[(i-1)*14 + j] = current_color
             else:
                 pixels[(i - 1) * 14 + j] = (0, 0, 0)
+            pixels.show()
     
     '''
     for i in num:
@@ -88,15 +117,94 @@ def change(root=0):
 
     t = datetime.now().strftime('%M')
     t = int(t) % 10
-    if 1 or t != pr or root:
+    if t != pr or root:
         print('t', t)
         change_digit(t)
         pr = t
 
+def show_time():
+    hour = datetime.now().strftime('%H')
+    minute = datetime.now().strftime('%M')
+    wait = 0.5
+    
+    change_digit(hour[0])
+    time.sleep(wait)
+    change_digit(hour[1])
+    time.sleep(wait)
+    change_digit(minute[0])
+    time.sleep(wait)
+    change_digit(minute[1])
+    time.sleep(wait)
+
+
+async def get_weather():
+    try:
+        data = []
+        with open('weather.txt', 'r') as f:
+            for line in f:
+                data.append(line)
+        last_time = int(data[0])
+        last_temp = data[1].replace('\n', '')
+        
+        #time_now = datetime.timestamp() 
+        dt = datetime.now()
+        time_now = time.mktime(dt.timetuple())
+        time_now = int(time_now)
+        print(time_now)
+        print(last_time)
+        print(last_temp)
+        temp = 0
+
+        if time_now - last_time < 30*60:
+            print('last')
+            temp = last_temp
+        else:
+            print('new')
+            resp = requests.get(url, headers=headers)
+            
+            resp = resp.json()
+            print(resp)
+            last_temp = resp['fact']['temp']
+            last_temp = str(last_temp)
+
+            with open('weather.txt', 'w') as f:
+                f.write(str(time_now) + '\n')
+                f.write(last_temp)
+
+            temp = last_temp
+        
+        wait = 1
+        for i in temp:
+            change_digit(i)
+            time.sleep(wait)
+    except Exception as e:
+        print(e)
+
+
+async def Wheel(WheelPos):
+    if(WheelPos < 85):
+        return (WheelPos * 3, 255 - WheelPos * 3, 0)
+    elif(WheelPos < 170):
+        WheelPos -= 85
+        return (255 - WheelPos * 3, 0, WheelPos * 3)
+    else:
+        WheelPos -= 170
+        return (0, WheelPos * 3, 255 - WheelPos * 3)
+
+
+async def rainbowCycle(wait=0.002):
+    for j in range(256):
+        print(j)
+        for i in range(100):
+            pixels[i] = await Wheel((int(i * 256 / 100) + j) & 255)
+    
+        pixels.show()
+        #await asyncio.sleep(wait)
+
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!", reply_markup=kb)
+    await message.reply("Привет!\nОтправляй мне эмодзи-сердечки и я буду менять цвет ленты в цвет отправленного сердечка", reply_markup=kb)
 
 
 @dp.message_handler(content_types=['text'])
@@ -115,8 +223,13 @@ async def main_logic(msg):
         with open('color.txt', 'w') as f:
             f.write(current_color)
         
-        change(1)
+        show_time()
+        #change(1)
         print(current_color)
+    elif msg.text == '🌈':
+        await rainbowCycle(2)
+    elif msg.text == '🌡':
+        await get_weather()
     else:
         try:
             li = msg.text.split()
@@ -150,9 +263,21 @@ def start_bot():
 
 def timer():
     global pr
+    wait = 1.5
     while True:
         if True:
-            change()
+            print('year')
+
+            year = datetime.now().strftime('%Y')
+            
+            change_digit(year[0])
+            time.sleep(wait)
+            change_digit(year[1])
+            time.sleep(wait)
+            change_digit(year[2])
+            time.sleep(wait)
+            change_digit(year[3])
+            time.sleep(wait)
             '''
             t = datetime.now().strftime('%M')
             t = int(t) % 10
@@ -164,7 +289,7 @@ def timer():
             
 
         #await ayncio.sleep(1)
-        time.sleep(5)
+        time.sleep(15)
 
 
 if __name__ == '__main__':
